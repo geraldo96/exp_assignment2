d6769833ce65cee2c1dc543aab738c00a25b839f
Geraldo96 <geraldomargjini@gmail.com>
2020-12-11 22:36:19 +0000

    cv.py quasi completo , da controllare i vari if nel passaggio nello stato sleep e per fsm.py bisogna ricontrollare lo stato sleep, launch file modificato

--- scripts/fsm.py
+++ scripts/fsm.py
@@ -1,39 +1,30 @@
 #!/usr/bin/env python 
 
 import rospy
-from fsm_receiver.msg import Command
-from position_receiver.msg import Position
 import smach
 import smach_ros
 import time
 import random
 import roslib
 from math import atan2
+from std_msgs.msg import Bool
+import numpy as np
+from scipy.ndimage import filters
+import cv2
+import imutils
 
 from geometry_msgs.msg import Twist,Point
 from nav_msgs.msg import Odometry
 from tf.transformations import euler_from_quaternion
+from sensor_msgs.msg import CompressedImage
+
 
 x = 0.0
 y = 0.0
 theta = 0.0
-
-
 Move_to_play = False
 
-def talker():
-    pub = rospy.Publisher('chatter_position', Position, queue_size=10)
-    #rospy.init_node('talker_from_fsm', anonymous=True)
-    r = rospy.Rate(10) #10hz
-    msg = Position()
-    msg.x = 4
-    msg.y = 4
-    i = 0
-    while i<10 :
-	i = i +1
-        rospy.loginfo(msg)
-        pub.publish(msg)
-        r.sleep()
+
 
 
 
@@ -41,10 +32,12 @@ def callback(data):
      global Move_to_play
 
      #rospy.loginfo("Mode is :%d ,User is in position: X = %d , Y = %d" % (data.state, data.x, data.y))
-     if  data.state == 1:
-		Move_to_play=1
+     if  data.data == True:
+        Move_to_play=True
      else :
-		Move_to_play=0
+        Move_to_play=False
+
+
 
 def newOdom(msg):
 	global x
@@ -59,19 +52,6 @@ def newOdom(msg):
 		
 
 
-def user_action():
-	global Move_to_play
-	
-	rospy.Subscriber("chatter_command", Command, callback)
-	if Move_to_play == 1:
-		Move_to_play  = 0;
-		return 'play'
-	else :
-		return 'sleep_from_normal'
-
-
-
-
     # define state Normal
 class Normal(smach.State):
     def __init__(self):
@@ -83,30 +63,32 @@ class Normal(smach.State):
         
     def execute(self, userdata):
 
-    #--- Code to move randomly ---
+            #--- Code to move randomly ---
     
 	    #rospy.init_node('controller_normal_randomness', anonymous=True)
 	    mov_pub = rospy.Publisher('/robot/cmd_vel', Twist, queue_size=10)
+            movement_cmd = Twist()
+	    
+            r = rospy.Rate(10) #10hz
 
-	    movement_cmd = Twist()
-	    r = rospy.Rate(10) #10hz
+            rospy.Subscriber("robot/detectblob",Bool, callback)
 
-	    #flag_ball_detected=0
+            
 
-            while userdata.normal_counter_in < 4:
+            while Move_to_play == False:
 
-            	movement_cmd.linear.x =(random.randint(0, 20))/10
+                rospy.Subscriber("robot/detectblob",Bool, callback)
+            	movement_cmd.linear.x =(random.randint(0, 15))/10
             	movement_cmd.angular.z =random.randint(-2, 2)
-            	rospy.loginfo('Sto dando valori random, dovrei muovermi')
+            	#rospy.loginfo('Sto dando valori random, dovrei muovermi')
             	mov_pub.publish(movement_cmd)
             	r.sleep()
-            	flag_ball_detected=+1
-            	time.sleep(3)
-                rospy.loginfo('Executing state NORMAL (users = %f)'%userdata.normal_counter_in)
-                userdata.normal_counter_out = userdata.normal_counter_in + 1 
 
-
-	    return 'sleep'
+            movement_cmd.linear.x = 0
+            movement_cmd.angular.z = 0
+            mov_pub.publish(movement_cmd)
+            rospy.loginfo('Trovato Qualcosa : Move to PLAY')
+            return 'play'
 
 
 
@@ -139,24 +121,22 @@ class Sleep(smach.State):
 
     		angle_to_goal = atan2(inc_y, inc_x)
 
-    		if abs(angle_to_goal - theta) > 0.3:
+    		if abs(angle_to_goal - theta) > 0.1:
     			speed.linear.x = 0.0
     			speed.angular.z = 1.0
     		else:
     			speed.linear.x = 0.5
     			speed.angular.z = 0.0
-    		#if abs(inc_x)<0.1 & abs(inc_y)<0.1:
-    			#speed.linear.x = 0.0
-    			#speed.angular.z = 0.0
-    			#flag_home=1
+    		if (abs(inc_x)<0.1) & abs(inc_y)<0.1 :
+    			speed.linear.x = 0.0
+    			speed.angular.z = 0.0
+    			flag_home=1
 
     		pub_vel.publish(speed)
     		r.sleep()
 
 
 		time.sleep(20) #after reaching the home he sleep for 20 sec.
-		rospy.loginfo('Executing state SLEEP (users = %f)'%userdata.sleep_counter_in)
-		userdata.sleep_counter_out = userdata.sleep_counter_in + 1 
 		return 'normal'
 
 	
@@ -166,25 +146,22 @@ class Sleep(smach.State):
 class Play(smach.State):
     def __init__(self):
         smach.State.__init__(self, 
-                             outcomes=['sleep_from_play'],
+                             outcomes=['sleep'],
                              input_keys=['play_counter_in'],
                              output_keys=['play_counter_out'])
         
-	self.sensor_input = 0
-        self.rate = rospy.Rate(200)  # Loop at 200 Hz
+
+    
 
     def execute(self, userdata):
-	
-        # simulate we recived the play command
-        while not rospy.is_shutdown():  
-            time.sleep(1)
-	    if self.sensor_input < 5: 
-		rospy.loginfo('Executing state PLAY (users = %f)'%userdata.play_counter_in)
-		userdata.play_counter_out = userdata.play_counter_in + 1
-		talker() 
-		return 'normal_from_play'
-	    self.sensor_input += 1 
-	    self.rate.sleep
+        sub_state = rospy.Publisher('/robot/state', Bool, queue_size=1)
+        while Move_to_play == True:
+            #rospy.loginfo('non sono nel loop di play')
+            sub_state.publish(True)
+            rospy.Subscriber("robot/detectblob",Bool, callback)
+            
+        return 'sleep'
+            
 	
 
         
@@ -194,25 +171,25 @@ def main():
 
     # Create a SMACH state machine
     sm = smach.StateMachine(outcomes=['container_interface'])
-    sm.userdata.sm_counter = 0
+    sm.userdata.sm_ball_detect = 0
 
     # Open the container
     with sm:
         # Add states to the container
 	smach.StateMachine.add('NORMAL', Normal(), 
                                transitions={'play':'PLAY','sleep':'SLEEP'},
-                               remapping={'normal_counter_in':'sm_counter',
-                                          'normal_counter_out':'sm_counter'})
+                               remapping={'normal_counter_in':'sm_ball_detect',
+                                          'normal_counter_out':'sm_ball_detect'})
 
         smach.StateMachine.add('SLEEP', Sleep(), 
                                transitions={'normal':'NORMAL'},
-                               remapping={'sleep_counter_in':'sm_counter', 
-                                          'sleep_counter_out':'sm_counter'})
+                               remapping={'sleep_counter_in':'sm_ball_detect', 
+                                          'sleep_counter_out':'sm_ball_detect'})
         
 	smach.StateMachine.add('PLAY', Play(), 
-                               transitions={'sleep_from_play':'SLEEP'},
-                               remapping={'play_counter_in':'sm_counter',
-                                          'play_counter_out':'sm_counter'})
+                               transitions={'sleep':'SLEEP'},
+                               remapping={'play_counter_in':'sm_ball_detect',
+                                          'play_counter_out':'sm_ball_detect'})
 
 
     # Create and start the introspection server for visualization
